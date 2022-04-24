65ee6a5149730250cd99d8a645335091a64c550c
Omkar Koli <koliomkar363@gmail.com>
2022-04-24 12:13:28 +0530

    Add an error handler

--- factorial/urls.py
+++ factorial/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:num>/', views.index, name='index'),
+    path('<str:input>/', views.error, name='error'),
 ]
--- factorial/views.py
+++ factorial/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request, num):
     result = math.factorial(num)
     return HttpResponse(result)
+
+
+def error(request, input):
+    return HttpResponse("Please enter a valid number!")
